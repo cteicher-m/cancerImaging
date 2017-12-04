df5249ded00d6cb314fb45133337f1df2ec429c2
Katherine Victoria Harrison <katherine@Katherines-MacBook-Pro-2.local>
2017-12-04 16:45:46 -0500

    Changes to classifier

--- tuning.py
+++ tuning.py
@@ -1,1 +1,18 @@
 # Tuning parameters
+# Choosing training number
+numberVector = []
+accuracyVectorRF = []
+accuracyVectorNB = []
+for i in range(1,14):
+    numberVector.append(i*23)
+    aRF = accuracy(randomForest.randomForestFunction(i*7, features, diagnosisBinary), diagnosisBinary)
+    accuracyVectorRF.append(aRF)
+    aNB = accuracy(gaussianNaiveBayes.gaussianBayes(i*7, features, diagnosisBinary), diagnosisBinary)
+    accuracyVectorNB.append(aNB)
+    
+#plt.plot(numberVector, accuracyVector)
+plt.plot(numberVector, accuracyVectorRF, 'b-', label='Random Forest')
+plt.plot(numberVector, accuracyVectorNB, 'g-', label='Random Naive Bayes')
+plt.ylabel("Accuracy")
+plt.xlabel("Training Number")
+plt.show()
