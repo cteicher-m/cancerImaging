799133ffedcdaa4df47314f1a28d96887e242952
Caroline Teicher <CarolineTeicher@Caroline-Teichers-MacBook-Pro.local>
2017-12-04 20:22:44 -0500

    knn and knn_modified working

--- knn.py
+++ knn.py
@@ -17,8 +17,6 @@ import matplotlib.pyplot as plt
 import base # random case
 import randomForest
 import gaussianNaiveBayes
-import knn
-import knn_modified
 import naiveBayes
 import supportVector
 
@@ -35,56 +33,43 @@ features = np.loadtxt('features.txt')
 
 
 # use Euclidean distance to measure differences
-def euclideanDistance(loc1, loc2, length):
-    distance = 0
-    for x in range(length):
-        distance += pow((loc1[x] - loc2[x]), length)
-    return math.sqrt(distance)
+# def euclideanDistance(loc1, loc2, length):
+#     distance = 0
+#     for x in range(length):
+#         distance += pow((loc1[x] - loc2[x]), length)
+#     return math.sqrt(distance)
 
-def getResult(trainNumber, features, groundTruth, k):
+def findNeighbors(trainNumber, features, groundTruth, k):
 
     results = []
     groundTruth = np.reshape(groundTruth,(322,))
-    f = features
-    gT = groundTruth
+    # f = features
+    # gT = groundTruth
 
     # Trim lists for training
-    features = features[:trainNumber,:]
-    groundTruth = groundTruth[:trainNumber]
+    X_train = features[:trainNumber]
+    Y_train = groundTruth[:trainNumber]
     
     # Trim lists for testing
-    f = f[trainNumber:,:]
-    gT = gT[trainNumber:]
+    X_test = features[trainNumber:]
+    Y_test = groundTruth[trainNumber:]
 
-    # print("features")
-    # print(features)
-    # print("f")
-    # print(f)
-    distances = []
     # return 2d matrix of each euclidean distance with respect to two indexes
-    for i in range(len(f)):
-        #print(i, euclideanDistance(features[i],f[i],6))
-        distances.append(euclideanDistance(features[i],f[i],6))
-    # distances = distance.cdist(features, f, 'euclidean')
+  
+    distances = distance.cdist(X_test, X_train, 'euclidean')
     # print("distances", distances)
-    sortedidxs = np.argsort(distances)
-    # return k nearest indexes
-    knn_idxs = sortedidxs[:k]
-    print("indexes")
-    print(knn_idxs)
-    # vote based on number in k nearest
-    # print(features)
-    vote = np.sum(features[knn_idxs]) 
-    if vote >= len(features) / 2:
-        results.append(1.0)
-    else:
-        results.append(1.0)
-    print(results)
-    return results
 
-def findNeighbors(trainNumber, features, groundTruth, k):
-    for i in range(trainNumber):
-        getResult(200, features[i], groundTruth[i], 2)
+    for i in range(len(X_test)):
+        distancestoXTrain = distances[i]
+        sortedidxs = np.argsort(distancestoXTrain)
+        knn_idxs = sortedidxs[:k]
+
+        vote = np.sum(Y_train[knn_idxs]) 
+        if vote >= k/2.0:
+            results.append(1.0)
+        else:
+            results.append(0.0)
+    return results
 
 
 print(findNeighbors(200,features, diagnosisBinary, 10))
